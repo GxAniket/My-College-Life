510c8011cb1f9e5ac6ddaa7c30216b76ee543a6c
Aniket Sundriyal <sundriyalaniket@gmail.com>
2026-03-28 23:29:09 +0530

    Update CarController.cs

--- CarController.cs
+++ CarController.cs
@@ -1,25 +1,74 @@
 using UnityEngine;
 
-public class CarControllerPhysics : MonoBehaviour
+public class CarController : MonoBehaviour
 {
-    public float speed = 1000f;
-    public float turnSpeed = 15f;
-    private Rigidbody rb;
+    public float motorForce = 1500f;
+    public float steeringAngle = 30f;
+    public float brakeForce = 3000f;
 
-    void Start()
+    public WheelCollider frontLeftWheel, frontRightWheel;
+    public WheelCollider rearLeftWheel, rearRightWheel;
+
+    public Transform frontLeftTransform, frontRightTransform;
+    public Transform rearLeftTransform, rearRightTransform;
+
+    private float moveInput;
+    private float steerInput;
+    private float currentBrakeForce;
+    private bool isBraking;
+
+    void Update()
     {
-        rb = GetComponent<Rigidbody>();
+        moveInput = Input.GetAxis("Vertical");
+        steerInput = Input.GetAxis("Horizontal");
+        isBraking = Input.GetKey(KeyCode.Space);
     }
 
     void FixedUpdate()
     {
-        float move = Input.GetAxis("Vertical");
-        float turn = Input.GetAxis("Horizontal");
+        HandleMotor();
+        HandleSteering();
+        UpdateWheels();
+    }
 
-        // Forward motion
-        rb.AddRelativeForce(Vector3.forward * move * speed * Time.fixedDeltaTime);
+    void HandleMotor()
+    {
+        frontLeftWheel.motorTorque = moveInput * motorForce;
+        frontRightWheel.motorTorque = moveInput * motorForce;
 
-        // Turning
-        transform.Rotate(0, turn * turnSpeed * Time.fixedDeltaTime, 0);
+        currentBrakeForce = isBraking ? brakeForce : 0f;
+        ApplyBraking();
+    }
+
+    void ApplyBraking()
+    {
+        frontLeftWheel.brakeTorque = currentBrakeForce;
+        frontRightWheel.brakeTorque = currentBrakeForce;
+        rearLeftWheel.brakeTorque = currentBrakeForce;
+        rearRightWheel.brakeTorque = currentBrakeForce;
+    }
+
+    void HandleSteering()
+    {
+        float steer = steeringAngle * steerInput;
+        frontLeftWheel.steerAngle = steer;
+        frontRightWheel.steerAngle = steer;
+    }
+
+    void UpdateWheels()
+    {
+        UpdateSingleWheel(frontLeftWheel, frontLeftTransform);
+        UpdateSingleWheel(frontRightWheel, frontRightTransform);
+        UpdateSingleWheel(rearLeftWheel, rearLeftTransform);
+        UpdateSingleWheel(rearRightWheel, rearRightTransform);
+    }
+
+    void UpdateSingleWheel(WheelCollider collider, Transform transform)
+    {
+        Vector3 pos;
+        Quaternion rot;
+        collider.GetWorldPose(out pos, out rot);
+        transform.position = pos;
+        transform.rotation = rot;
     }
 }
